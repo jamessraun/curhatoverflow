288795ae8fb0c533807a9fde6358b69ecfd8bb02
jamessraun <jeezybig@gmail.com>
2017-05-25 15:49:30 +0700

    add views

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,29 @@
 
 
 var login = (req,res) => {
+  res.render('login')
+}
+
+var home = (req,res) => {
   res.render('home')
 }
 
+var signup = (req,res) => {
+  res.render('signup')
+}
+
+var profile = (req,res) => {
+  res.render('profile')
+}
+
+var mycurhat = (req,res) => {
+  res.render('mycurhat')
+}
 
 module.exports = {
-  login:login
+  login:login,
+  home:home,
+  signup:signup,
+  profile:profile,
+  mycurhat:mycurhat
 };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ var express = require('express')
 var router = express.Router()
 var controller = require('../controllers/controller')
 
-router.get('/',controller.login)
+router.get('/login',controller.login)
+router.get('/signup',controller.signup)
+router.get('/',controller.home)
+router.get('/profile',controller.profile)
+
+router.get('/mycurhat',controller.mycurhat)
+
 
 
 module.exports = router;
